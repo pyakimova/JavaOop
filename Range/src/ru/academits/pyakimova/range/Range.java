e4bd3dbbd33ae05160e455d2237a156911bfa655
home <yakimovapa@bcs.ru>
2022-08-22 00:42:51 +0700

    Adds getIntersection to Range

--- Range/src/ru/academits/pyakimova/range/Range.java
+++ Range/src/ru/academits/pyakimova/range/Range.java
@@ -1,4 +1,64 @@
 package ru.academits.pyakimova.range;
 
 public class Range {
+    private double from;
+    private double to;
+
+    public Range(double from, double to) {
+        if (from < to) {
+            this.from = from;
+            this.to = to;
+        } else {
+//нужно сгенерить иксепшн
+//предполагаем, что точка не является интервалом
+        }
+    }
+
+    public double getFrom() {
+        return from;
+    }
+
+    public void setFrom(double from) {
+        if (from < to) {
+            this.from = from;
+        } else {
+//нужно сгенерить иксепшн
+        }
+    }
+
+    public double getTo() {
+        return to;
+    }
+
+    public void setTo(double to) {
+        if (from < to) {
+            this.to = to;
+        } else {
+//нужно сгенерить иксепшн
+        }
+    }
+
+    public double getLength() {
+        return to - from;
+    }
+
+    public boolean isInside(double value) {
+        return value >= from && value <= to;
+    }
+
+    public Range getIntersection(Range range1, Range range2) {
+        if (range1.to <= range2.from || range2.to <= range1.from) {
+            return null;
+        }
+
+        return new Range(Math.max(range1.from,range2.from),Math.min(range1.to,range2.to));
+    }
+
+    public Range getSum(Range range1, Range range2) {
+        return range1;
+    }
+
+    public Range getDifference(Range range1, Range range2) {
+        return range2;
+    }
 }
--- Range/src/ru/academits/pyakimova/range_main/RangeMain.java
+++ Range/src/ru/academits/pyakimova/range_main/RangeMain.java
@@ -1,4 +1,19 @@
 package ru.academits.pyakimova.range_main;
 
+import ru.academits.pyakimova.range.Range;
+
 public class RangeMain {
+    public static void main(String[] args) {
+        Range range = new Range(1.1, 3);
+
+        System.out.println("Длина диапазона: " + range.getLength());
+
+        range.setFrom(1.9);
+        System.out.println("Левая граница диапазона: " + range.getFrom());
+
+        range.setTo(2.9);
+        System.out.println("Правая граница диапазона: " + range.getTo());
+
+        System.out.println("Лежит ли точка 1 внутри диапазона: " + range.isInside(1));
+    }
 }
